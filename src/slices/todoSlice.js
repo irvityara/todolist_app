f1bcd8de1c53d270e987184f2391696e5ccbb1ec
Ipikk <irvityara456@gmail.com>
2023-10-30 06:12:08 +0700

    slice

---
+++ src/slices/todoSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState: {
+        todos: [],
+    },
+    reducers: {
+        addTodos: (state, action) => {
+            const newTodo = {
+                todo: action.payload.text,
+            };
+            state.todos = [...state.todos, newTodo];
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo === action.payload.id);
+        },
+        searchTodos: (state, action) => {
+            state.filteredTodos = state.todos.filter((todo) =>
+              todo.content.startsWith(action.payload.search)
+            );
+          },
+    }
+})
+
+export const { addTodos, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
