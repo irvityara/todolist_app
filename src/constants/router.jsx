6ad7468e6a7b60d336f7827a84c6b454678aef15
Ipikk <irvityara456@gmail.com>
2023-10-30 23:54:18 +0700

    commit

--- src/constants/router.jsx
+++ src/constants/router.jsx
@@ -7,7 +7,7 @@ const router = createBrowserRouter([
     path: "/",
         element:
             <div style={{ display: "grid", justifyContent: "center", alignItems: "center", marginTop: "80px", marginBottom: "70px" }}>
-                <h1>What's the plan for today?</h1>
+                <h1>What`s the plan for today?</h1>
                 <TodoPage />
             </div>
   },
